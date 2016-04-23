0a0043de83eb5067d47d227627271ab56c92e95d
budsakon55 <pandorlarh@gmail.com>
2016-04-23 17:34:13 +0700

    #02 สร้างยานสีเขียวตรงกลาง

---
+++ file/GameEngine.java
@@ -0,0 +1,36 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.Timer;
+
+
+public class GameEngine {
+	GamePanel gp;
+		
+		
+	private SpaceShip v;	
+	
+	
+	
+	public GameEngine(GamePanel gp, SpaceShip v) {
+		this.gp = gp;
+		this.v = v;		
+		
+		gp.sprites.add(v);
+        
+        gp.updateGameUI();
+        
+        		
+    }
+}
+  
+
+
+
+
--- file/Main.java
+++ file/Main.java
@@ -1,15 +1,24 @@
-package f2.spw;
+
 
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
-public class Main {
+public class Main{
+	
 	public static void main(String[] args){
+		
 		JFrame frame = new JFrame("Space War");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 650);
+		frame.setSize(400,600);
 		frame.getContentPane().setLayout(new BorderLayout());
+		SpaceShip v = new SpaceShip(180, 550, 20, 20);
+		GamePanel gp = new GamePanel();
+		gp.updateGameUI();
+        GameEngine engine = new GameEngine(gp, v);
+		frame.getContentPane().add(gp, BorderLayout.CENTER);
+		
+		
 		frame.setVisible(true);
-			}
+	}
 }
